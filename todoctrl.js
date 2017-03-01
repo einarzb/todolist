6d2d38cc6ac1f8bf38e2e2381af5a7df7f5be6ab
einarzb <einarzb@gmail.com>
2017-03-01 16:52:17 +0200

    changed checkbox behavior onto function and some styling

--- todoctrl.js
+++ todoctrl.js
@@ -8,21 +8,33 @@ app.controller('toDoCtrl', function($scope, toDoService) {
        value1 : true,
        value2 : false
      };
+
+  $scope.checked = false;
    //boolean
 
   //creats object and send it to service
   $scope.createTask = function(){
      var newTask = {task: $scope.userTask, rate:$scope.userRate, check:$scope.userCheckboxModel.value1};
-     //console.log(newTask);
      toDoService.addTaskToList(newTask); //invoke service function 
-  };
+     $scope.userTask = "";
+     $scope.userRate = "";
+    };
 
   //finds item to remove from the array and send it to service
-  $scope.removeFromList = function (task) {
-    var index = $scope.toDoEmpty.indexOf(task);
-    console.log("task is: " + task.name);
-    console.log("index of task is: " + index);
+  $scope.removeFromList = function (index) {
     toDoService.removeTaskFromList(index);
-  }
+  };
+
+  $scope.checkBox = function(){
+    if($scope.checked){
+      $scope.checked = false;
+      console.log('you changed the ole check thing')
+      console.log($scope.checked)
+    }else{
+      $scope.checked = true;
+      console.log('you changed the ole check thing')
+      console.log($scope.checked)
+    }
+  };
 
 });
